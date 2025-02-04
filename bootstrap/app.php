d06cccb0bbb83dec8b0b682d293033718635834f
firmansyah-alf <alfirman887@gmail.com>
2025-02-04 09:57:36 +0700

    hm

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,24 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+/*
+ |--------------------------------------------------------------------------
+ | Create The Application
+ |--------------------------------------------------------------------------
+ |
+ | The first thing we will do is create a new Laravel application instance
+ | which serves as the "glue" for all the components of Laravel, and is
+ | the IoC container for the system binding all of the various parts.
+ |
+ */
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+// Override lokasi storage agar menggunakan direktori /tmp (writable di Vercel)
+$app->useStoragePath('/tmp/storage');
+
+// Selanjutnya, kamu bisa menambahkan konfigurasi atau binding lain yang dibutuhkan.
+// Misalnya, load environment, konfigurasi log, dsb.
+
+return $app;
