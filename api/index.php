9f1e5b0cc237c89e65459046287dcfa96824b959
firmansyah-alf <alfirman887@gmail.com>
2025-02-07 22:51:15 +0700

    oke

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
 // Pastikan kamu mengubah path jika diperlukan
 // Adjust the relative path if necessary
 require __DIR__ . '/../vendor/autoload.php';
-// $app = require_once __DIR__ . '/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 // Then proceed to handle the request...
 
 
