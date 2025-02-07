448823174b3854fb781e68196605a39f4de817f9
firmansyah-alf <alfirman887@gmail.com>
2025-02-07 22:29:15 +0700

    changes in vercel build and nothing else

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    base: "/",
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
