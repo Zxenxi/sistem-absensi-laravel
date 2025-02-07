89af2b77f2aee2c04d13c3625d8f52d90fcf52c2
firmansyah-alf <alfirman887@gmail.com>
2025-02-07 22:19:19 +0700

    bootstrap on anjay

--- api/index.php
+++ api/index.php
@@ -4,16 +4,16 @@
 // Pastikan kamu mengubah path jika diperlukan
 // Adjust the relative path if necessary
 require __DIR__ . '/../vendor/autoload.php';
-// $app = require_once __DIR__ . '/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 // Then proceed to handle the request...
 
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// $response->send();
+$response->send();
 
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
